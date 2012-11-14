52b31963d5bfe1ffd9e7769961561ff34b4c98b8
git <sebastianclaici@gmail.com>
2012-11-14 11:39:33 +0000

    Implemented equals for Product

--- src/com/example/quick_shop/Product.java
+++ src/com/example/quick_shop/Product.java
@@ -11,4 +11,11 @@ public class Product
 	public String toString() {
 		return name;
 	}
+	
+	public boolean equals(Object o) {
+		if (!(o instanceof Product))
+			return false;
+		Product prod = (Product) o;
+		return prod.name.equals(name);
+	}
 }
