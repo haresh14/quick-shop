6a7ca944b2d29bbd346fef1a2d4097298706f4da
git <sebastianclaici@gmail.com>
2012-11-17 00:27:56 +0000

    Rewrote parseJSON to remove extraneous code. Will continue working on it when I have time.

--- src/com/example/quick_shop/ParseJSON.java
+++ src/com/example/quick_shop/ParseJSON.java
@@ -4,130 +4,61 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.NoSuchElementException;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class ParseJSON
-{
-	
-	public static void getProducts(InputStream fpath, Store store, Category cat)
+{	
+	public static void getProducts(InputStream fpath, Store store, Category category)
 	{
 		//Read the input file
-		String source = "";
-		try
-		{
-			source = readFile(fpath);
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-			return;
-		}
+		String source = ParseJSON.readFile(fpath);
 		
-		//Write the string to a JSONObject
 		JSONObject json;
-		try
-		{
+		try {
 			json = new JSONObject(source);
 		}
-		catch (JSONException e)
-		{
+		catch (JSONException e) {
 			e.printStackTrace();
 			return;
 		}
 		
-		
-		//Create a JSONArray of results
 		JSONArray results = null;
-		
 		try {
 			results = (JSONArray) json.get("results");
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
+			System.out.println("Something went wrong parsing JSON");
 			e.printStackTrace();
 		}
 		
-		//Add products to the category, and then add stores/prices to products.
 		try {
 			JSONObject o = null;
-			for(int i = 0; i < results.length(); i++)
-			{
+			for(int i = 0; i < results.length(); i++) {
 				o = (JSONObject) results.get(i);
-				//System.out.println(o.get("consumer_product/topic:name") + " " + (int) ((Double) o.get("consumer_product/msrp/amount")*100));
 				String name = (String) o.get("consumer_product/topic:name");
-				name = normaliseBreadName(name);
-				Product prod = new Product(name);
-				cat.addProduct(prod); //add prod
-				String symbol = "";
-				try
-				{
-					symbol = (String) o.get("consumer_product/msrp/currency/prefix_symbol");
-				}
-				catch (JSONException e) {}
-				int amount = 0;
-				if(symbol.contains("£")) {
-					if (o.get("consumer_product/msrp/amount") instanceof Double)
-						amount = (int)Math.round((((Double) o.get("consumer_product/msrp/amount")))) * 100;
-					if (o.get("consumer_product/msrp/amount") instanceof Integer)
-						amount =((Integer) o.get("consumer_product/msrp/amount")) * 100;
-				} else {
-					amount = (Integer)o.get("consumer_product/msrp/amount");
-				}
+				Product product = new Product(name);
+				category.addProduct(product);
 			}
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Something went wrong parsing JSON");
+		}
+	}
+	
+	private static String readFile(InputStream in) {
+		String line = null;
+		StringBuilder buffer = new StringBuilder();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		try {
+			while ((line = reader.readLine()) != null) {
+				buffer.append(line);
+			}
+		} catch(IOException e) {
+			System.out.println("Something went wrong while reading");
 			e.printStackTrace();
 		}
-	}
-	
-	private static String normaliseBreadName(String name)
-	{
-		String newname = name.toLowerCase();
-		if(newname.contains("0g"))
-		{
-			String nameStuff[] = newname.split("0g");
-			//System.out.println(nameStuff[0] + "0G");
-			newname = nameStuff[0] + "0g";
-		}
-		newname = capitalizeString(newname);
-		return newname;
-	}
-	
-	private static String readFile(InputStream filePath) throws java.io.IOException
-	{
-		StringBuffer fileData = new StringBuffer(1000);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(filePath));
-		char[] buf = new char[1024];
-		int numRead=0;
-		while((numRead=reader.read(buf)) != -1)
-		{
-			String readData = String.valueOf(buf, 0, numRead);
-			fileData.append(readData);
-			buf = new char[1024];
-		}
-		reader.close();
-		return fileData.toString();
-	}
-	
-	public static String capitalizeString(String string)
-	{
-		  char[] chars = string.toLowerCase().toCharArray();
-		  boolean found = false;
-		  for (int i = 0; i < chars.length; i++)
-		  {
-		    if (!found && Character.isLetter(chars[i]))
-		    {
-		      chars[i] = Character.toUpperCase(chars[i]);
-		      found = true;
-		    }
-		    else if (Character.isWhitespace(chars[i]) || chars[i]=='.' || chars[i]=='\'')
-		    {
-		      found = false;
-		    }
-		  }
-		  return String.valueOf(chars);
+		return buffer.toString();
 	}
 }
